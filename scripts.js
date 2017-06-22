4d5213b9625e051240aa1a98f0c4f31a5d711887
ivanzusko <ivan.zusko@gmail.com>
2017-06-22 22:05:14 +0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+/**
+ * You can use the following snippet (just uncomment it) 
+ * or anything else.
+ * 
+ * @example
+ * - Uncomment the following snippet;
+ * - open extansions managment page in your Chrome;
+ * - Click `Load unpacked extension...`;
+ * - select your folder;
+ * - your extension will appear among others;
+ * - click the yellow icon `T`;
+ * - you will see small popup with bold `Chrome Extension Boilerplate` and small sign `Example` below.
+ */
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   // here the logic lives
+//   document.getElementById('output').textContent = 'Example'
+// });
